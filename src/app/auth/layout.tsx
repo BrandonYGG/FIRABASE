655f3f9bd08ac1794196df9ee3ae06f30e86eab2
Brandon Yahir González González <branyahir03@gmail.com>
2025-10-10 16:03:57 +0000

    este componente, muevelo un poco mas porque esta en el limite (_for elem

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-        <div className="absolute top-4 left-4">
+        <div className="absolute top-6 left-6">
              <Link href="/" className="flex items-center space-x-2 text-foreground">
                 <Construction className="h-6 w-6 text-primary" />
                 <span className="font-bold font-headline">OrderFlow Construct</span>
