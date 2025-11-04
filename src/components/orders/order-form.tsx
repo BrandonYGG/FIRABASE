266f1f9a4753879a9e94aad862197f0147ad9757
Brandon Yahir González González <branyahir03@gmail.com>
2025-11-04 17:13:32 +0000

    esto se va actualizando conforme se coloca la cantidad de material  (_fo

--- src/components/orders/order-form.tsx
+++ src/components/orders/order-form.tsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/navigation';
 import { useState, useMemo, useEffect } from 'react';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
-import { addDoc, collection, Timestamp, updateDoc } from 'firebase/firestore';
+import { addDoc, collection, Timestamp, updateDoc, doc } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 import { Button } from '@/components/ui/button';
